a406619f8bcb10ed585e8545d9e3496bc921c91b
Athul P. Girija <athulpg007@gmail.com>
2022-05-09 10:03:03 -0600

    Add launch vehicle performance data, add example 68

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -8,28 +8,31 @@ Basic test for AMAT.launcher
 import unittest
 
 try:
-    from AMAT.launcher import Launcher
+	from AMAT.launcher import Launcher
 except ModuleNotFoundError:
-    raise ModuleNotFoundError("Cannot import Launcher from AMAT.Launcher")
+	raise ModuleNotFoundError("Cannot import Launcher from AMAT.Launcher")
+
 
 class TestLauncher(unittest.TestCase):
 
-	def test_create_launcher(self):
-
+	def test_atlas_V_401(self):
 		"""
 		Performance query for a few launch vehicles.
 		"""
 
-		Launcher1 = Launcher('atlasV401')
-		Launcher2 = Launcher('atlasV551')
-		Launcher3 = Launcher('falconH')
-		Launcher4 = Launcher('sls-block-1B')
-
+		launcher = Launcher('atlasV401', datafile='../launcher-data/atlas-v401.csv')
 		# Compare with numbers from https://elvperf.ksc.nasa.gov/Pages/Query.aspx
-		self.assertAlmostEqual(Launcher1.performanceQuery(16), 2088, delta=20)
-		self.assertAlmostEqual(Launcher2.performanceQuery(16), 4501, delta=20)
-		self.assertAlmostEqual(Launcher3.performanceQuery(16), 11004, delta=20)
-		self.assertAlmostEqual(Launcher4.performanceQuery(100), 5039, delta=100)
+		self.assertAlmostEqual(launcher.launchMass(16), 2088, delta=20)
+
+
+	def test_atlas_V_551(self):
+		"""
+		Performance query for a few launch vehicles.
+		"""
+
+		launcher = Launcher('atlasV401', datafile='../launcher-data/atlas-v551.csv')
+		# Compare with numbers from https://elvperf.ksc.nasa.gov/Pages/Query.aspx
+		self.assertAlmostEqual(launcher.launchMass(16), 4325, delta=20)
 
 if __name__ == '__main__':
 	unittest.main()
