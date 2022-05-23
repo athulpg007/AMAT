42886f7aaa2d54a8d2a910ef281a9476038c3307
Athul P. Girija <athulpg007@gmail.com>
2022-05-22 19:12:48 -0600

    Update AMAT to v2.2.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2022, A.P. Girija'
 author = 'Athul P. Girija'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.2.9'
+release = 'v2.2.10'
 
 
 # -- General configuration ---------------------------------------------------
