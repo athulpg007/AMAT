e397cda513a276a8b3383e70ccab6ab294658b69
Athul P. Girija <athulpg007@gmail.com>
2022-09-27 00:12:05 -0600

    Update AMAT to v2.2.18

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='AMAT',
-      version='2.2.17',
+      version='2.2.18',
       description='Aerocapture Mission Analysis Tool',
       url='https://github.com/athulpg007/AMAT',
       long_description=long_description,
