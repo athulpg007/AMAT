991c94229da6818fe3d1896ad77d3fb099423769
Athul P. Girija <athulpg007@gmail.com>
2022-12-28 19:58:06 -0700

    Add jplephem==2.17 to requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(name='AMAT',
       author_email='athulpg007@gmail.com',
       license='GPL-3.0-or-later',
       packages=['AMAT'],
-      install_requires=['numpy==1.21.6', 'scipy==1.8.0', 'matplotlib==3.5.2', 'pandas==1.4.2', 'astropy==4.3.1'],
+      install_requires=['numpy==1.21.6', 'scipy==1.8.0', 'matplotlib==3.5.2', 'pandas==1.4.2',
+                        'astropy==4.3.1', 'jplephem==2.17'],
       classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
